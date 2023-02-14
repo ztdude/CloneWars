9e581c979df7b76318971d4384b5e2f0c3ef1250
Skywing1889 <123699199+Skywing1889@users.noreply.github.com>
2023-02-14 14:02:26 -0800

    Update player.py

--- player.py
+++ player.py
@@ -17,9 +17,9 @@ class Player(pygame.sprite.Sprite):
   def input(self):
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_right]:
+    if keys[pygame.K_RIGHT]:
       self.direction.x = 1
-    elif keys[pygame.K_left]:
+    elif keys[pygame.K_LEFT]:
       self.direction.x = -1
     else:
       self.direction.x = 0
