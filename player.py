041163779e5db13a7ab325499c3d065496944ebb
Skywing1889 <123699199+Skywing1889@users.noreply.github.com>
2023-02-14 13:56:31 -0800

    Update player.py

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-  def __init__(sel.pos.groups):
+  def __init__(self,pos,groups):
     super().__init__(groups)
     self.image = pygame.Surface((TILE.SIZE // 2,TILE_SIZE))
     self.image.fill(PLAYER_COLOR)
