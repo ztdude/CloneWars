246770cae587eb28670a655c7fc4f463aac16923
Skywing1889 <123699199+Skywing1889@users.noreply.github.com>
2023-03-09 13:42:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,27 @@ from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+#Backround
+background_image = pygame.image.load("asssets/Backround.jpeg")
+
+def make_bg():
+  scaled_bg = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+  screen.blit(scaled_bg, (0,0))
+
 pygame.display.set_caption('Clone Wars Platformer')
 clock = pygame.time.Clock()
 
 level = Level()
-
-
 
 while True: 
   for event in pygame.event.get():
        if event.type == pygame.QUIT:
          pygame.quit()
          sys.exit()
-    
-    
-  screen.fill(BG_COLOR)
 
+  make_bg()
+  
   level.run()
 
   pygame.display.update()
   clock.tick(60)
-
-
-
